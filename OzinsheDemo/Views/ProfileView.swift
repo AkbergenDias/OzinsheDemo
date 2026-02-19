23ec4f14079713e7f503cf319f3bc4dd04470be3
AkbergenDias <akbergen.dias.s@gmail.com>
2026-02-19 23:52:54 +0500

    Still making Settings tab

--- OzinsheDemo/Views/ProfileView.swift
+++ OzinsheDemo/Views/ProfileView.swift
@@ -44,12 +44,13 @@ class ProfileView: UIView {
         
         label.snp.makeConstraints { (make) in
             make.top.equalTo(imageView.snp.bottom).offset(24)
-            make.bottom.equalTo(emailLabel.snp.top).offset(8)
+            make.centerX.equalToSuperview()
         }
         
         emailLabel.snp.makeConstraints { (make) in
             make.top.equalTo(label.snp.bottom).offset(8)
             make.bottom.equalToSuperview().offset(-24)
+            make.centerX.equalToSuperview()
         }
         
         return view
@@ -57,21 +58,87 @@ class ProfileView: UIView {
     }()
     
     // TODO: Continue this
-//    lazy var personalDataButton = {
-//        let button = UIButton(type: .custom)
-//        
-//        button.setTitle("Жеке деректер", for: .normal)
-//        button.titleLabel?.font = UIFont(name:"SFProDisplay-Bold", size: 16)
-//        button.setTitleColor(.white, for: .normal)
-//        button.backgroundColor = UIColor(named: "7E2DFC")
-//        button.isEnabled = false
-//        button.isHidden = true
-//        
-//        return button
-//        
-//        
-//        
-//    }()
+    lazy var personalDataButton = {
+        let view = UIView()
+        let button = UIButton(type: .custom)
+        let subtitle = UILabel()
+        let accessoryView = UIImageView(image: UIImage(named: "Chevron-Right-Outline"))
+        
+        button.setTitle("Жеке деректер", for: .normal)
+        button.titleLabel?.font = UIFont(name:"SFProDisplay-Medium", size: 16)
+        button.setTitleColor(UIColor(named: "111827"), for: .normal)
+        button.backgroundColor = .white
+        button.titleLabel?.textAlignment = .left
+        
+        subtitle.text = "Өңдеу"
+        subtitle.font = UIFont(name:"SFProDisplay-Medium", size: 12)
+        subtitle.textColor = UIColor(named: "9CA3AF")
+        subtitle.textAlignment = .center
+        
+        view.addSubview(button)
+        view.addSubview(subtitle)
+        view.addSubview(accessoryView)
+        
+        button.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.height.equalTo(64)
+            make.width.equalToSuperview()
+        }
+        
+        subtitle.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(24)
+            make.bottom.equalToSuperview().offset(-24)
+            make.right.equalToSuperview().offset(-24)
+        }
+        
+        accessoryView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(24)
+            make.bottom.equalToSuperview().offset(-24)
+            make.right.equalToSuperview()
+        }
+        
+        return view
+        
+    }()
+    
+    lazy var passwordChangeButton = {
+        let view = UIView()
+        let button = UIButton(type: .custom)
+        let accessoryView = UIImageView(image: UIImage(named: "Chevron-Right-Outline"))
+        
+        button.setTitle("Құпия сөзді өзгерту", for: .normal)
+        button.titleLabel?.font = UIFont(name:"SFProDisplay-Medium", size: 16)
+        button.setTitleColor(UIColor(named: "111827"), for: .normal)
+        button.backgroundColor = .white
+        button.titleLabel?.textAlignment = .left
+        
+        view.addSubview(button)
+        view.addSubview(accessoryView)
+        
+        button.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.height.equalTo(64)
+            make.width.equalToSuperview()
+        }
+        
+        accessoryView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(24)
+            make.bottom.equalToSuperview().offset(-24)
+            make.right.equalToSuperview()
+        }
+        
+        return view
+        
+    }()
+    
+    lazy var bottomView = {
+        let view = UIView()
+        
+        view.backgroundColor = UIColor(named: "D1D5DB")
+        
+        
+        return view
+    }()
 
     
     override init(frame: CGRect) {
@@ -87,12 +154,34 @@ class ProfileView: UIView {
     func setupUI(){
         
         addSubview(profileHeaderView)
+        addSubview(personalDataButton)
+        addSubview(passwordChangeButton)
+        addSubview(bottomView)
         
         profileHeaderView.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(24)
+            make.top.equalToSuperview().offset(132)
+            make.centerX.equalToSuperview()
             
         }
         
+        personalDataButton.snp.makeConstraints { (make) in
+            make.top.equalTo(profileHeaderView.snp.bottom).offset(24)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        passwordChangeButton.snp.makeConstraints { (make) in
+            make.top.equalTo(personalDataButton.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        bottomView.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.right.equalToSuperview().inset(24)
+            make.left.equalToSuperview().inset(24)
+
+        }
         
         
     }
