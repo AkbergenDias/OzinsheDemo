863b3ee963fa665bf03e4905741b01727566fa42
AkbergenDias <akbergen.dias.s@gmail.com>
2026-02-25 14:00:59 +0500

    Settings button rows

--- OzinsheDemo/Views/ProfileView.swift
+++ OzinsheDemo/Views/ProfileView.swift
@@ -68,7 +68,7 @@ class ProfileView: UIView {
         button.titleLabel?.font = UIFont(name:"SFProDisplay-Medium", size: 16)
         button.setTitleColor(UIColor(named: "111827"), for: .normal)
         button.backgroundColor = .white
-        button.titleLabel?.textAlignment = .left
+        button.contentHorizontalAlignment = .left
         
         subtitle.text = "Өңдеу"
         subtitle.font = UIFont(name:"SFProDisplay-Medium", size: 12)
@@ -110,7 +110,7 @@ class ProfileView: UIView {
         button.titleLabel?.font = UIFont(name:"SFProDisplay-Medium", size: 16)
         button.setTitleColor(UIColor(named: "111827"), for: .normal)
         button.backgroundColor = .white
-        button.titleLabel?.textAlignment = .left
+        button.contentHorizontalAlignment = .left
         
         view.addSubview(button)
         view.addSubview(accessoryView)
@@ -131,12 +131,100 @@ class ProfileView: UIView {
         
     }()
     
-    lazy var bottomView = {
+    lazy var languageSelectButton = {
         let view = UIView()
+        let button = UIButton(type: .custom)
+        let subtitle = UILabel()
+        let accessoryView = UIImageView(image: UIImage(named: "Chevron-Right-Outline"))
         
+        button.setTitle("Тіл", for: .normal)
+        button.titleLabel?.font = UIFont(name:"SFProDisplay-Medium", size: 16)
+        button.setTitleColor(UIColor(named: "111827"), for: .normal)
+        button.backgroundColor = .white
+        button.contentHorizontalAlignment = .left
+        
+        subtitle.text = "Қазақша"
+        subtitle.font = UIFont(name:"SFProDisplay-Medium", size: 12)
+        subtitle.textColor = UIColor(named: "9CA3AF")
+        subtitle.textAlignment = .center
+        
+        view.addSubview(button)
+        view.addSubview(subtitle)
+        view.addSubview(accessoryView)
+        
+        button.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.height.equalTo(64)
+            make.width.equalToSuperview()
+        }
+        
+        subtitle.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(24)
+            make.bottom.equalToSuperview().offset(-24)
+            make.right.equalToSuperview().offset(-24)
+        }
+        
+        accessoryView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(24)
+            make.bottom.equalToSuperview().offset(-24)
+            make.right.equalToSuperview()
+        }
+        
+        return view
+        
+    }()
+    
+    lazy var toggleSwitch: UISwitch = {
+       let toggle = UISwitch()
+        toggle.onTintColor = UIColor(named: "B376F7")
+        toggle.isOn = false
+        return toggle
+    }()
+    
+    lazy var darkModeButton = {
+        let view = UIView()
+        let button = UIButton(type: .custom)
+        let accessoryView = UIImageView(image: UIImage(named: "Chevron-Right-Outline"))
+        
+        button.setTitle("Қараңғы режим", for: .normal)
+        button.titleLabel?.font = UIFont(name:"SFProDisplay-Medium", size: 16)
+        button.setTitleColor(UIColor(named: "111827"), for: .normal)
+        button.backgroundColor = .white
+        button.contentHorizontalAlignment = .left
+        
+        view.addSubview(button)
+        view.addSubview(toggleSwitch)
+        
+        button.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.height.equalTo(64)
+            make.width.equalToSuperview()
+        }
+        
+        toggleSwitch.snp.makeConstraints { (make) in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview()
+        }
+        
+        return view
+        
+    }()
+    
+    lazy var bottomView1 = {
+        let view = UIView()
         view.backgroundColor = UIColor(named: "D1D5DB")
-        
-        
+        return view
+    }()
+    
+    lazy var bottomView2 = {
+        let view = UIView()
+        view.backgroundColor = UIColor(named: "D1D5DB")
+        return view
+    }()
+    
+    lazy var bottomView3 = {
+        let view = UIView()
+        view.backgroundColor = UIColor(named: "D1D5DB")
         return view
     }()
 
@@ -156,7 +244,11 @@ class ProfileView: UIView {
         addSubview(profileHeaderView)
         addSubview(personalDataButton)
         addSubview(passwordChangeButton)
-        addSubview(bottomView)
+        addSubview(languageSelectButton)
+        addSubview(darkModeButton)
+        addSubview(bottomView1)
+        addSubview(bottomView2)
+        addSubview(bottomView3)
         
         profileHeaderView.snp.makeConstraints { (make) in
             make.top.equalToSuperview().offset(132)
@@ -176,11 +268,35 @@ class ProfileView: UIView {
             make.horizontalEdges.equalToSuperview().inset(24)
         }
         
-        bottomView.snp.makeConstraints { make in
+        languageSelectButton.snp.makeConstraints { (make) in
+            make.top.equalTo(passwordChangeButton.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        darkModeButton.snp.makeConstraints { (make) in
+            make.top.equalTo(languageSelectButton.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.height.equalTo(64)
+        }
+        
+        bottomView1.snp.makeConstraints { make in
             make.height.equalTo(1)
-            make.right.equalToSuperview().inset(24)
-            make.left.equalToSuperview().inset(24)
-
+            make.top.equalTo(personalDataButton.snp.bottom)
+            make.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        bottomView2.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.top.equalTo(passwordChangeButton.snp.bottom)
+            make.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        bottomView3.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.top.equalTo(languageSelectButton.snp.bottom)
+            make.horizontalEdges.equalToSuperview().inset(24)
         }
         
         
